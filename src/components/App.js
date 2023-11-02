363e46942b06e2ad3102ff3c84f1ede8b6a142bb
+ <jkunins95@gmail.com>
2023-11-02 14:57:24 -0400

    added state, changled line 22

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
-import React from "react";
+// add a click event (Delete) button - need to add useState
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
+
 
 import { CATEGORIES, TASKS } from "../data";
 // console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [categories, setCategories] = useState(CATEGORIES);
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList tasks={TASKS} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
