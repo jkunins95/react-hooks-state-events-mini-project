914a1b0fdaed70f8bdfed4edb487696b95716323
+ <jkunins95@gmail.com>
2023-11-02 14:51:42 -0400

    added alternative ways to add in text and category

--- src/components/Task.js
+++ src/components/Task.js
@@ -10,4 +10,14 @@ function Task({task}) {
   );
 }
 
+// function Task({ task, category }) {
+//   return (
+//     <div className="task">
+//       <div className="label">{category}</div>
+//       <div className="text">{text}</div>
+//       <button className="delete">X</button>
+//     </div>
+//   );
+// }
+
 export default Task;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,10 +5,20 @@ function TaskList({ tasks }) {
   return (
     <div className="tasks">
       {tasks.map(task => (
-        <Task task={task} />
+        // need to add a key prop (since there is no ID available, can just use "text" as the unique identifier)
+        <Task key={task.text} task={task} />
       ))}
     </div>
   );
 }
+
+//   return (
+//     <div className="tasks">
+//       {tasks.map(task => (
+//         <Task task={task.text} category={task.category} />
+//       ))}
+//     </div>
+//   );
+// }
 
 export default TaskList;
